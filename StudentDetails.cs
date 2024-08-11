8cdfb664e0ee0640590240c9247cd5ca88fc8531
Sunkavilli Suvarna raju <19A91F0018@aec.edu.in>
2024-08-11 10:35:12 +0530

    Added-Emp-Details

---
+++ StudentDetails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace DotNetPractice
+{
+    internal class StudentDetails
+    {
+        static void Main(string[] args)
+        {
+            //Ask the user to Enter Student Details
+            Console.WriteLine("Enter Student Details");
+            Console.WriteLine("Enter Registration Number");
+            int RegdNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Name");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Enter Marks of three Subjects:");
+            Console.WriteLine("Subject1");
+            int Mark1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Subject2");
+            int Mark2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Subject3");
+            int Mark3 = Convert.ToInt32(Console.ReadLine());
+
+            int TotalMarks = Mark1 + Mark2 + Mark3;
+            int AverageMark = TotalMarks / 3;
+
+            //Display the Student Details
+            Console.WriteLine("\nStudent Details are as Follows:");
+            Console.WriteLine($"Registration Number: {RegdNumber}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Total Marks : {TotalMarks}");
+            Console.WriteLine($"Average Mark: {AverageMark}");
+            Console.ReadKey();
+        }
+    }
+}
