49e0f744b51d7c4d57875651376665bbc55676ae
Sunkavilli Suvarna raju <19A91F0018@aec.edu.in>
2024-08-16 21:54:34 +0530

    Updates Topics

---
+++ VariablesDemo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractice
+{
+
+   internal class VariablesDemo
+    {
+        static int x = 10;
+        int y = 20;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(x);
+        }
+
+    }
+}
