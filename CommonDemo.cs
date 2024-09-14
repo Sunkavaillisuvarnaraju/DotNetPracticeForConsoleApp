66f195be502b0095b9893ca4b8238ee50ca81760
Sunkavilli Suvarna raju <19A91F0018@aec.edu.in>
2024-09-14 18:31:30 +0530

    Comments Resolved

---
+++ CommonDemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractice
+{
+    public class CommonDemo
+    {
+        static CommonDemo()
+        {
+            Console.WriteLine("Constructor");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
---
+++ DelegateExample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractice
+{
+    public delegate void SomeMethod(string  name);
+    public class DelegateExample
+    {
+        public static void Main(string[] args)
+        {
+            SomeMethod sm = new SomeMethod(SomeName);
+            sm.Invoke("Raju");
+            Console.ReadLine();
+        }
+        public static void SomeName(string name)
+        {
+            Console.WriteLine($"Hello : {name}");
+        }
+    }
+}
