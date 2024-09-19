cab5cd55fd7b896daf69d3f12a91f3bf11542c70
Sunkavilli Suvarna raju <19A91F0018@aec.edu.in>
2024-09-19 11:06:55 +0530

    Added Source code

---
+++ EmployeeProperty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractice
+{
+    public class EmployeeProperty
+    {
+        public static void Main(string[] args)
+        {
+            PropertiesExample example = new PropertiesExample();
+            example.EmpId = 101;
+            example.EmpName = "Raju";
+            Console.WriteLine($"Employee Id :{example.EmpId}");
+            Console.WriteLine($"Employee Name : {example.EmpName}");
+            Console.ReadLine();
+
+        }
+    }
+}
