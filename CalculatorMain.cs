b0d4302aa3f6697e5719a46af55b865bafe2cd48
Sunkavilli Suvarna raju <19A91F0018@aec.edu.in>
2024-09-22 14:14:13 +0530

    Added New Files

---
+++ CalculatorMain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractice
+{
+    public class CalculatorMain
+    {
+        public static void Main(string[] args)
+        {
+            CalculatorProperties cp = new CalculatorProperties();
+            Console.WriteLine("Enter First Number :");
+            cp.SetNumber1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number :");
+            cp.SetNumber2= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Result o");
+        }
+    }
+}
---
+++ CalculatorProperties.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractice
+{
+    public class CalculatorProperties
+    {
+        int _Number1, _Number2, _Result;
+
+        public int SetNumber1
+        {
+            set
+            {
+                _Number1 = value;
+            }
+            
+        }
+
+        public int SetNumber2
+        {
+            set
+            {
+                _Number2 = value;
+            }
+          
+        }
+        public int GetResult
+        {
+            
+            get
+            {
+                return _Result;
+            }
+        }
+    }
+}
