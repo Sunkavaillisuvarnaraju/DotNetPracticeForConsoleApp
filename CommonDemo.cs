63809b086bb7fc06e715bc1fd4a91c79a17267a9
Sunkavilli Suvarna raju <19A91F0018@aec.edu.in>
2024-09-22 14:08:06 +0530

    Updated

---
+++ CommonDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractice
+{
+    public class CommonDemo
+    {
+       public CommonDemo() 
+        {
+            Console.WriteLine(" Constructor is called");
+        }
+        ~CommonDemo() 
+        {
+            Console.WriteLine(" Destructor is called ");
+        }
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Main Method Execution Started");
+            CommonDemo obj1 = new CommonDemo();
+            CommonDemo obj2 = new CommonDemo();
+            obj1 =null;
+            GC.Collect();
+
+            Console.WriteLine("Main Method Execution is Ended");
+            Console.ReadLine();
+        }
+    }
+}
