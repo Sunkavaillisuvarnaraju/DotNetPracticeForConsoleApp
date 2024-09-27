1acffa1fa8f20680644299bb763f10a23d05878c
Sunkavilli Suvarna raju <19A91F0018@aec.edu.in>
2024-09-27 21:05:29 +0530

    LatestCodeWithMultiThreading

---
+++ MultiThreadingExamples/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreadingExamples
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = Thread.CurrentThread;
+            t.Name = "Main Thread";
+            Console.WriteLine("Name of the Thread :"+t.Name);
+            Console.WriteLine("Name of the current Thread :"+Thread.CurrentThread.Name);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MultiThreadingExamples/SingleThreadDrawBackExample.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreadingExamples
+{
+    public class SingleThreadDrawBackExample
+    {
+        public static void Main(string[] args)
+        {
+            SingleThreadDrawBackExample example = new SingleThreadDrawBackExample();
+            example.M1();
+            example.M2();
+            Console.ReadLine();
+        }
+
+        public void M1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);   
+                Thread.Sleep(5000);
+            }
+        }
+        public void M2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
