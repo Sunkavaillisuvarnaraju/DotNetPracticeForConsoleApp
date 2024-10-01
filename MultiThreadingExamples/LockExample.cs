0935c24c8c6009a35186d5eefab02fda9cde1131
Sunkavilli Suvarna raju <19A91F0018@aec.edu.in>
2024-10-01 19:02:12 +0530

    DoneDev2Mod

--- MultiThreadingExamples/LockExample.cs
+++ MultiThreadingExamples/LockExample.cs
@@ -25,6 +25,8 @@ namespace MultiThreadingExamples
 
             Console.WriteLine(count);
             Console.ReadLine();
+            Console.ReadLine();
+            Console.ReadKey();
 
         }
 
