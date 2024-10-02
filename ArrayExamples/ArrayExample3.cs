afbdbc5382cd9fc5f6679b8b643629ae7e860887
Sunkavilli Suvarna raju <19A91F0018@aec.edu.in>
2024-10-02 20:47:28 +0530

    Updated  with Array Examples

---
+++ ArrayExamples/ArrayExample3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExamples
+{
+    public class ArrayExample3
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Size of an Array");
+            int arr = Convert.ToInt32(Console.ReadLine());
+            int[] array1 = new int[arr];
+            Console.WriteLine();
+            Console.WriteLine($"Size of an Array Entered by the user is : {arr}");
+            Console.WriteLine();
+            Console.WriteLine("Enter Elements to Store into an Array");
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = Convert.ToInt32(Console.ReadLine());    
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write($"Elements Entered by the User is :");
+            foreach (int i in array1)
+            {
+                Console.Write("\t"+i);   
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MultiThreadingExamples/MonitorExample.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace MultiThreadingExamples
+{
+    public class MonitorExample
+    {
+        private static object locker = new object();
+        public static void Main(string[] args)
+        {
+            Thread t1 = new Thread(PrintNumbers)
+            {
+                Name = "Thread 1"
+            };
+            Thread t2 = new Thread(PrintNumbers)
+            {
+                Name = "Thread 2"
+            };
+            Thread t3 = new Thread(PrintNumbers)
+            {
+                Name = "Thread 3"
+            };
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            Console.ReadLine();
+        }
+
+        public static void PrintNumbers()
+        {
+             
+            Console.WriteLine(Thread.CurrentThread.Name + "Trying to Enter into Method");
+            try
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + "Trying to Enter into try block");
+                Monitor.Enter(locker);
+                Console.WriteLine(Thread.CurrentThread.Name + "Enterted into Critical section");
+                for (int i = 1; i <= 5; i++)
+                {
+                    Thread.Sleep(2000);
+                    Console.Write(i + ",");
+                }
+                Console.WriteLine();
+            }
+                finally
+            {
+                Monitor.Exit(locker);
+                Console.WriteLine(Thread.CurrentThread.Name+"Exit from the Critical section");
+            }
+        }
+
+
+    }
+}
