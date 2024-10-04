2fc87c6c1d6e63e06aa9c0e05eb48c26c5f9ea4c
Sunkavilli Suvarna raju <19A91F0018@aec.edu.in>
2024-10-04 15:36:10 +0530

    ArraysCommitUptoJaggedArray

---
+++ ArrayExamples/JaggedArrayExample.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExamples
+{
+    public class JaggedArrayExample
+    {
+        public static void Main(string[] args)
+        {
+            string[] empname = new string[3];
+            empname[0] = "Raju";
+            empname[1] = "Rani";
+            empname[3] = "Ramu";
+
+            //Jagged Array
+            string[][] jaggedArray = new string[3][];
+            jaggedArray[0] = new string[3];
+            jaggedArray[1] = new string[1];
+            jaggedArray[2] = new string[3];
+
+            jaggedArray[0][0] = "PG";
+            jaggedArray[0][1] = "bachlore";
+            jaggedArray[0][2] = "Inter";
+
+            jaggedArray[1][0] = "PG";
+
+            jaggedArray[2][0] = "PG";
+            jaggedArray[2][1] = "Inter";
+            jaggedArray[2][2] = "Doctorate";
+
+
+        }
+    }
+}
---
+++ ArrayExamples/TwoDimensionalArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//2-Dimenssional Array
+namespace ArrayExamples
+{
+    public class TwoDimensionalArray
+    {
+        public static void Main(string[] args)
+        {
+            int[,] arr1 = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+                                //OR
+
+            int[,] arr2 = new int[2, 3]
+            {
+                {13,14,13 },
+                { 2,5,6}
+            };
+
+            Console.WriteLine(arr2.GetLength(2));
+            Console.ReadLine();
+        }
+    }
+}
